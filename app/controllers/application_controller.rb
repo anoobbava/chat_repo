0a973a9e5e808d743c182e879828407ad3115e78
anoob <anoob.bava@gmail.com>
2015-09-14 11:10:35 +0530

    before implementing the facebook gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
  def after_sign_in_path_for(resource)
     users_path
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_sanitized_params, if: :devise_controller?
   
     def configure_sanitized_params
-      # devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:firstname, :designation_id ,:middlename, :previous_experiance_year,:previous_experiance_month,:lastname, :email, :username, :password, :password_confirmation, :previous_experiance,:empid, :dob, :timezone, :doj,  :gender, :education, :comments, :locked, :deactivated, :reason, :phone,:deactivated_date,:image)}
+     # devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:firstname, :designation_id ,:middlename, :previous_experiance_year,:previous_experiance_month,:lastname, :email, :username, :password, :password_confirmation, :previous_experiance,:empid, :dob, :timezone, :doj,  :gender, :education, :comments, :locked, :deactivated, :reason, :phone,:deactivated_date,:image)}
       # devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:remove_image,:firstname, :designation_id ,:middlename, :lastname, :email, :username,  :empid, :dob, :timezone, :doj,  :gender, :education, :comments, :locked, :deactivated, :reason, :phone,  :deactivated_date,:image)}
       devise_parameter_sanitizer.for(:sign_in) {|u| u.permit( :email,:password,)}
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users
+  #, :controllers => {:registrations => "registrations"}
   resources :users 
   root 'users#home'
   # get '/users/:id/user_index' => 'users#user_index'
   get 'users/'=>'users#home'
-  get 'users/signup_success'=>'users#signup_success'
+  #get 'users/signup_success'=>'users#signup_success'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
